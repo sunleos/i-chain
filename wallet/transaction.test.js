a310f48265af9f9f2cd846de88a408b246ef7129
sunleos <vhulchenko@gmail.com>
2018-07-10 00:51:03 +0300

    Create a Transaction & Testing

---
+++ wallet/transaction.test.js
@@ -0,0 +1,34 @@
+const Transaction = require ('./transaction');
+const Wallet = require ('./index');
+
+describe ('Transaction', () => {
+  let transaction, wallet, recepient, amount;
+
+  beforeEach (() => {
+    wallet = new Wallet();
+    amount = 50;
+    recepient = 'r3c1p13nt';
+    transaction = Transaction.newTransaction(wallet, recepient, amount);
+  });
+
+  it ('outputs the `amount` subtracted from the wallet balance', () => {
+    expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+     .toEqual(wallet.balance - amount);
+  });
+
+  it ('outputs the `amount` added to the recepient', () => {
+    expect(transaction.outputs.find(output => output.address === recepient).amount)
+     .toEqual(amount);
+  });
+
+  describe ('transacting with an amount that exceeds the balance', () => {
+    beforeEach (() => {
+      amount = 50000;
+      transaction = Transaction.newTransaction(wallet, recepient, amount);
+    });
+  
+    it ('does not create the transaction', () => {
+      expect(transaction).toEqual(undefined);
+    });
+  });
+});
