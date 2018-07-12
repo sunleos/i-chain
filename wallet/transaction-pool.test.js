3f29cffda7dc981a46b3282b3cf920bf37cd8ba1
sunleos <vhulchenko@gmail.com>
2018-07-13 01:44:19 +0300

    Transaction Pool & Testing

---
+++ wallet/transaction-pool.test.js
@@ -0,0 +1,28 @@
+const TransactionPool = require ('./transaction-pool');
+const Transaction = require ('./transaction');
+const Wallet = require ('./index');
+
+describe ('TransactionPool', () => {
+  let tp, wallet, transaction;
+
+  beforeEach (() => {
+    tp = new TransactionPool();
+    wallet = new Wallet();
+    transaction = Transaction.newTransaction(wallet, 'r4nd-4dr355', 30);
+    tp.updateOrAddTransaction(transaction);
+  });
+
+  it ('add a transaction to the pool', () => {
+    expect (tp.transactions.find (t => t.id === transaction.id)).toEqual (transaction);
+  });
+
+  it ('updates a transaction to the pool', () => {
+    const oldTransaction = JSON.stringify(transaction);
+    const newTransaction = transaction.update (wallet, 'foo-4dd4re23', 40);
+    tp.updateOrAddTransaction(transaction);
+
+    expect (JSON.stringify(tp.transactions.find (t => t.id === newTransaction.id)))
+     .not. toEqual(oldTransaction)
+  });
+
+});
