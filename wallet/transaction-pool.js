3dc04059cca10f8388407eb939167b639151d671
sunleos <vhulchenko@gmail.com>
2018-07-16 00:18:09 +0300

    Create the Miner Class, grab valid transaction & Testing

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,5 @@
+const Transaction = require ('../wallet/transaction')
+
 class TransactionPool {
 
   constructor() {
@@ -17,6 +19,30 @@ class TransactionPool {
   existingTransaction (address) {
     return this.transactions.find(t => t.input.address === address);
   }
+
+  validTransactions () {
+    return this.transactions.filter (transaction => {
+     const outputTotal = transaction.outputs.reduce ((total, output) => {
+       return total + output.amount;
+     }, 0);
+
+     if(transaction.input.amount !== outputTotal) {
+       console.log(`Individual transaction from ${transaction.input.address}.`);
+       return;
+     }
+
+     if(!Transaction.verifyTransaction(transaction)) {
+      console.log(`Individual signature from ${transaction.input.address}.`);
+      return;
+     }
+
+     return transaction;
+
+    })
+  }
+
 }
+
+
 
 module.exports = TransactionPool;
